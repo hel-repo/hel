d10c1088bda50ea94749436859bfe9e52f2387f6
Fingercomp <fingercomp@users.noreply.github.com>
2016-11-03 15:35:12 +0700

    Removed teapot route

--- hel/__init__.py
+++ hel/__init__.py
@@ -133,7 +133,6 @@ def main(global_config, **settings):
     config.add_request_method(is_logged_in, 'logged_in', reify=True)
 
     # Setup routes
-    config.add_route('teapot', '/teapot')
     config.add_route('auth', '/auth')
     config.add_route('curuser', '/profile')
     config.scan()
